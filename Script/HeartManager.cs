ba085da7ecfe4ed119d5e1603b36b0de0c73b1ae
muhammadsastra <muhammadsastra123@gmail.com>
2020-11-27 06:29:23 -0800

    Post Pertama

---
+++ Script/HeartManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HeartManager : MonoBehaviour
+{
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite halFullHeart;
+    public Sprite empetyHeart;
+    public FloatValue heartContainer;
+    public FloatValue playerCurrentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitHearth();
+        
+    }
+    public void InitHearth()
+    {
+        for (int i = 0; i < heartContainer.initialValue; i++)
+        {
+            hearts[i].gameObject.SetActive(true);
+            hearts[i].sprite = fullHeart;
+        }
+    }
+    public void UpdateHearts()
+    {
+        float tempHealth = playerCurrentHealth.RuntimeValue / 2;
+        for (int i = 0; i < heartContainer.initialValue; i++)
+        {
+            if(i < tempHealth-1)
+            {
+                hearts[i].sprite = fullHeart;
+            }else if ( i >= tempHealth)
+            {
+                hearts[i].sprite = empetyHeart;
+
+            }else{
+                hearts[i].sprite = halFullHeart;
+            }
+        }
+    }
+}
